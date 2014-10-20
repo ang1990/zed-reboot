04a3c1c35647af2005bcf24663adb6f4b0034863
sapiangkasa <andika.nma@gmail.com>
2014-10-20 21:04:24 +0800

    explosion
    managed to make it work with zed but somehow didn't trigger with zombies

---
+++ Assets/Landmine.js
@@ -0,0 +1,10 @@
+﻿#pragma strict
+
+var prefab : GameObject;
+
+function OnCollisionEnter2D(coll : Collision2D) {
+	if (coll.gameObject.tag == "zed"){
+		Instantiate(prefab, transform.position, Quaternion.identity);
+		Destroy (gameObject);
+	}
+}
