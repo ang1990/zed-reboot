d76337f4352c3fad8b9a5d4fa4808b50eba6fe79
sapiangkasa <andika.nma@gmail.com>
2014-10-28 17:17:13 +0800

    edit in neon lights

---
+++ Assets/neonLight.js
@@ -0,0 +1,27 @@
+﻿#pragma strict
+
+var animator : Animator;
+
+function Start () {
+	animator.SetBool("on", true);
+}
+
+function Update () {
+	if(randomGenerate() < 6) {
+		if (animator.GetBool("flicker"))
+			animator.SetBool("flicker",false);
+		else
+			animator.SetBool("flicker",true);
+	}
+	
+	if(randomGenerate() < 8) {
+		if (animator.GetBool("on"))
+			animator.SetBool("on",false);
+		else
+			animator.SetBool("on",true);
+	}
+}
+
+function randomGenerate () {
+	return Random.Range(1,10);
+}
