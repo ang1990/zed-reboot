7f7b4982bc91ff041de6078e24bf0b6c67bb8af7
sapiangkasa <andika.nma@gmail.com>
2014-11-09 22:21:58 +0800

    wave overlay function
    function is done just need to add the prefab to the scene and call the
    function in the script appropriately

---
+++ Assets/waveOverlay.js
@@ -0,0 +1,60 @@
+﻿#pragma strict
+
+private var initFontSize : float;
+private var initFontA : float;
+
+function Start () {
+	gameObject.guiText.pixelOffset.x = Screen.width/2;
+	gameObject.guiText.pixelOffset.y = Screen.height/2;
+	gameObject.guiText.enabled = false;
+	initFontSize = gameObject.guiText.fontSize;
+	initFontA = gameObject.guiText.color.a;
+	waveEnd();
+}
+
+
+function waveStart (wave : int) {
+	gameObject.guiText.text = "Wave "+wave.ToString();
+	gameObject.guiText.enabled = true;
+	var scale : float = gameObject.guiText.fontSize/10;
+	gameObject.guiText.fontSize *= 3;
+	var i : int;
+	for (i=0; i < 20; i++) {
+		gameObject.guiText.fontSize -= scale;
+		yield WaitForSeconds(0.02);
+	}
+	
+	yield WaitForSeconds(0.5);
+	
+	while (gameObject.guiText.color.a != 0) {
+		gameObject.guiText.color.a -= 0.1;
+		yield WaitForSeconds(0.10);
+	}
+	gameObject.guiText.enabled = false;
+	
+	gameObject.guiText.fontSize = initFontSize;
+	gameObject.guiText.color.a = initFontA;
+}
+
+function waveEnd () {
+	gameObject.guiText.text = "Wave Cleared";
+	gameObject.guiText.enabled = true;
+	var scale : float = gameObject.guiText.fontSize/10;
+	gameObject.guiText.fontSize *= 3;
+	var i : int;
+	for (i=0; i < 20; i++) {
+		gameObject.guiText.fontSize -= scale;
+		yield WaitForSeconds(0.02);
+	}
+	
+	yield WaitForSeconds(0.5);
+	
+	while (gameObject.guiText.color.a != 0) {
+		gameObject.guiText.color.a -= 0.1;
+		yield WaitForSeconds(0.10);
+	}
+	gameObject.guiText.enabled = false;
+	
+	gameObject.guiText.fontSize = initFontSize;
+	gameObject.guiText.color.a = initFontA;
+}
